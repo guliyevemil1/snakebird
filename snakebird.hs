1789c8bd2fbd8db08fe392f9c7bcea1907a081c0
Emil <guliyevemil1@gmail.com>
2017-03-05 17:14:32 -0800

    Moved everything to be done via lens

--- snakebird.hs
+++ snakebird.hs
@@ -1,9 +1,14 @@
-import qualified Data.Map as M
+{-# LANGUAGE TemplateHaskell #-}
+
 import qualified Data.Set as S
 import Control.Monad
+import Control.Monad.State
+import Control.Applicative
+import Control.Lens
 
 type MyInt = Int
 data Point = Point MyInt MyInt deriving (Show, Eq, Ord)
+type Direction = Point
 
 down = Point 0 (-1)
 up = Point 0 1
@@ -12,56 +17,70 @@ right = Point 1 0
 
 instance Num Point where
     (Point x y) + (Point x' y') = Point (x+x') (y+y')
-    _ * _ = undefined
-    abs = undefined
     fromInteger n = Point (fromIntegral n) 0
     negate (Point x y) = Point (-x) (-y)
     signum = undefined
+    _ * _ = undefined
+    abs = undefined
 
 type PointSet = S.Set Point
 
-type Snakebird = [Point]
-
 data GameState = GameState {
-    snakebird :: Snakebird,
-    obstacles :: PointSet,
-    fruits :: PointSet,
-    goal :: Point
-} deriving (Show, Eq, Ord)
+        _snakebird :: [Point],
+        _obstacles :: PointSet,
+        _fruits :: PointSet,
+        _goal :: Point
+    } deriving (Show, Eq, Ord)
 
-side :: Point -> Snakebird -> [Point]
-side p s = filter (`elem` s) $ map (+p) s
+makeLenses ''GameState
+
+type Snakebird a = StateT GameState [] a
+
+side :: Point -> [Point] -> [Point]
+side p ps = filter (`elem` ps) $ map (+p) ps
 
 bottom = side down
 
-isEmpty :: GameState -> Point -> Bool
-isEmpty gs p@(Point x y) = and [y >= 0, not $ S.member p $ obstacles gs, not $ p `elem` (snakebird gs)]
+isEmpty :: Point -> Snakebird Bool
+isEmpty p@(Point x y) = do 
+    guard $ y >= 0 
+    hasNoObstacles <- not . S.member p <$> use obstacles
+    guard hasNoObstacles
+    doesNotContainSb <- not . (p `elem`) <$> use snakebird
+    guard doesNotContainSb
+    return True
 
-allEmpty :: GameState -> [Point] -> Bool
-allEmpty gs = null . filter (not . isEmpty gs)
+allEmpty :: [Point] -> Snakebird Bool
+allEmpty ps = and <$> mapM isEmpty ps 
 
-moveSnakebird :: Point -> GameState -> GameState
-moveSnakebird p gs = if allEmpty gs (side p (snakebird gs))
-    then moveSnakebird p $ gs { snakebird = map (+p) (snakebird gs) }
-    else gs
+moveSnakebird :: Point -> Snakebird ()
+moveSnakebird p = do
+    pointsAreEmpty <- use snakebird >>= allEmpty . side p
+    guard pointsAreEmpty
+    snakebird %= map (+p)
+    moveSnakebird p
 
 fall = moveSnakebird down
 
-moveHead :: GameState -> Point -> [GameState]
-moveHead gs p = do
-    guard $ isEmpty gs p
-    let hasFruit = S.member p (fruits gs)
-    let newFruits = (if not hasFruit then id else S.delete p) (fruits gs)
-    let newSnakebird = (if hasFruit then id else init) (p : snakebird gs)
-    return $ gs { fruits = newFruits, snakebird = newSnakebird }
+moveHead :: Point -> Snakebird ()
+moveHead p = do
+    canMove <- isEmpty p
+    guard $ canMove
+    hasFruit <- S.member p <$> use fruits
+    fruits %= (if not hasFruit then id else S.delete p)
+    snakebird %= (if hasFruit then id else init) . (p:)
 
-move :: GameState -> [GameState]
-move gs = do
-    dir <- [right, up, left, down]
-    moveHead gs dir
+move :: Snakebird ()
+move = do
+    dir <- lift [right, up, left, down]
+    p <- ((+dir) . head) <$> use snakebird
+    moveHead p
 
 main = do
-    print gs
-    mapM_ print (move gs)
+    print $ gs
+    putStrLn "Running once"
+    mapM_ print $ runStateT move gs
+    putStrLn "Running twice"
+    mapM_ print $ runStateT (move >> move) gs
     where
         gs = GameState [Point 0 0, Point (-1) 0] S.empty (S.singleton (Point 1 0)) (Point 1 2)
